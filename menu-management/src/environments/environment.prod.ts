644c6464c3b20fdd495dc4eb4746379ddc0722d4
wilberC <wilber.carrascal@gmail.com>
2020-08-05 03:12:23 -0500

    addProductionVariable

--- menu-management/src/environments/environment.prod.ts
+++ menu-management/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD6SkqjlTJea-azaVS_KKSS-EkttcT7Txk",
+    authDomain: "fitco-challenge.firebaseapp.com",
+    databaseURL: "https://fitco-challenge.firebaseio.com",
+    projectId: "fitco-challenge",
+    storageBucket: "fitco-challenge.appspot.com",
+    messagingSenderId: "129018060397",
+    appId: "1:129018060397:web:24ddd324569489c0736964",
+    measurementId: "G-WF07JVZV1B"
+  }
 };
